d6e7a95f8b40aadd1f03fcd26b4cf99de80b0c2d
Leo_huang <81244821+itLeo1105@users.noreply.github.com>
2022-01-20 23:55:33 +0800

    JSX语法

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 导入react 报
+import React from 'react'
+import  ReactDOM  from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// JSX创建元素
+const title = <h1>---JSX---</h1>
+
+ReactDOM.render(title,document.getElementById('root'))
